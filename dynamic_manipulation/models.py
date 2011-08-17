f5f18dade5b4a5a3396ca7a03b41274d4700c5d7
Aaron Madison <aaron.madison@theimtgroup.com>
2011-08-17 08:53:03 -0500

    specified content type model explicitly in generic foreign keys

--- dynamic_manipulation/models.py
+++ dynamic_manipulation/models.py
@@ -18,10 +18,10 @@ class ManipulationLog(models.Model):
 
     trigger_content_type = models.ForeignKey('contenttypes.ContentType', related_name="manipulation_triggers")
     trigger_model_id = models.PositiveIntegerField(db_index=True)
-    trigger_model = generic.GenericForeignKey(fk_field='trigger_model_id')
+    trigger_model = generic.GenericForeignKey(fk_field='trigger_model_id', ct_field='trigger_content_type')
 
     side_effect_content_type = models.ForeignKey('contenttypes.ContentType', related_name="manipulation_side_effects")
     side_effect_model_id = models.PositiveIntegerField(db_index=True)
-    side_effect_model = generic.GenericForeignKey(fk_field='side_effect_model_id')
+    side_effect_model = generic.GenericForeignKey(fk_field='side_effect_model_id', ct_field='side_effect_content_type')
 
     objects = ManipulationLogManager()
