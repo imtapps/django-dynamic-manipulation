be653327fd301394b0b0bc675c2daa952912278c
Aaron Madison <aaron.madison@theimtgroup.com>
2011-08-18 09:24:00 -0500

    removed dependencies files and updated setup.py

--- setup.py
+++ setup.py
@@ -1,32 +1,80 @@
-from setuptools import setup, find_packages
+
+import os
+import re
+from distutils.core import Command, setup
+from setuptools import find_packages
 
 from dynamic_manipulation import VERSION
 
-REQUIREMENTS = open('dependencies.txt', 'r').read().split('\n')
-TEST_REQUIREMENTS = open('test_dependencies.txt', 'r').read().split('\n')
+REQUIREMENTS = [
+    'django',
+    'django-dynamic-rules>=0.2.0',
+]
 
-setup(
-    name="django-dynamic-manipulation",
-    version=VERSION,
-    author="Matthew J Morrison & Aaron Madison",
-    author_email="mattjmorrison@mattjmorrison.com",
-    description="Record rule based dynamic manipulations.",
-    long_description=open('README.txt', 'r').read(),
-    url="https://github.com/imtapps/django-dynamic-manipulation",
-    packages=find_packages(exclude=["example"]),
-    install_requires=REQUIREMENTS,
-    tests_require=TEST_REQUIREMENTS,
-    test_suite='runtests.runtests',
-    zip_safe=False,
-    classifiers = [
-        "Development Status :: 3 - Alpha",
-        "Environment :: Web Environment",
-        "Framework :: Django",
-        "Intended Audience :: Developers",
-        "License :: OSI Approved :: BSD License",
-        "Operating System :: OS Independent",
-        "Programming Language :: Python",
-        "Topic :: Software Development",
-        "Topic :: Software Development :: Libraries :: Application Frameworks",
-    ],
-)
+TEST_REQUIREMENTS = [
+    'mock',
+    'lettuce',
+    'pyhamcrest',
+]
+
+def do_setup():
+    setup(
+        name="django-dynamic-manipulation",
+        version=VERSION,
+        author="Matthew J Morrison & Aaron Madison",
+        author_email="mattjmorrison@mattjmorrison.com",
+        description="Record rule based dynamic manipulations.",
+        long_description=open('README.txt', 'r').read(),
+        url="https://github.com/imtapps/django-dynamic-manipulation",
+        packages=find_packages(exclude=["example"]),
+        install_requires=REQUIREMENTS,
+        tests_require=TEST_REQUIREMENTS,
+        test_suite='runtests.runtests',
+        zip_safe=False,
+        classifiers = [
+            "Development Status :: 3 - Alpha",
+            "Environment :: Web Environment",
+            "Framework :: Django",
+            "Intended Audience :: Developers",
+            "License :: OSI Approved :: BSD License",
+            "Operating System :: OS Independent",
+            "Programming Language :: Python",
+            "Topic :: Software Development",
+            "Topic :: Software Development :: Libraries :: Application Frameworks",
+        ],
+        cmdclass={
+            'install_dev': InstallDependencies,
+            'uninstall_dev': UninstallDependencies,
+        },
+    )
+
+
+class PipDependencies(Command):
+    pip_command = ""
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def get_all_dependencies(self):
+        """
+        replace all > or < in the dependencies so the system does not
+        try to redirect stdin or stdout from/to a file.
+        """
+        command_line_deps = ' '.join(REQUIREMENTS + TEST_REQUIREMENTS)
+        return re.sub(re.compile(r'([<>])'), r'\\\1', command_line_deps)
+
+    def run(self):
+        os.system("pip %s %s" % (self.pip_command, self.get_all_dependencies()))
+
+class InstallDependencies(PipDependencies):
+    pip_command = 'install'
+
+class UninstallDependencies(PipDependencies):
+    pip_command = 'uninstall'
+
+if __name__ == '__main__':
+    do_setup()
